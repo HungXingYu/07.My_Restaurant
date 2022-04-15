7c1a0b4f1bc20542e741dbdfb25cb156655984b1
HungXingYu <clarehung0406@gmail.com>
2022-04-15 13:20:05 +0800

    feat: add module.exports

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 const restaurantSchema = new Schema({
     name: {
@@ -46,10 +46,10 @@ const restaurantSchema = new Schema({
         require: true
     },
 
-    uploadDate:{
-      type: String,
-      require: true
+    uploadDate: {
+        type: String,
+        require: true
     }
 })
 
-const Restaurants = mongoose.model("Restaurants", restaurantSchema);
+module.exports = mongoose.model("Restaurants", restaurantSchema)
