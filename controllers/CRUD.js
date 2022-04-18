fb9b475f94cd4dd0c5071531759da6a3805d3788
HungXingYu <clarehung0406@gmail.com>
2022-04-18 16:53:34 +0800

    refactor: modify crud 1. 將重複的CRUD獨立成一個檔案 >> 首頁及後台全部餐廳列出(findAll) >> 以餐廳名稱或餐廳類別搜尋餐廳結果(findRestaurantByFilter) >> 已id搜尋顯示該id詳細資料(findById) 2.修改handlebars接受資料來源的變數名稱

---
+++ controllers/CRUD.js
@@ -0,0 +1,42 @@
+const findAll = (Model, pageName, req, res) => {
+    Model.find()
+        .lean()
+        .sort({ _id: "asc" })
+        .then((results) => res.render(pageName, { results }))
+        .catch((error) => console.error(error))
+}
+
+const findRestaurantByFilter = (Model, pageName, req, res) => {
+    const keyword = req.query.keyword //* 從URL中傳來的 keyword引數
+    const reg = new RegExp(keyword, "i") //* 不區分大小寫
+
+    /** 多條件與模糊查詢指令
+     *  多條件查詢: query.$or
+     *  模糊查詢: query.$regex
+     */
+    Model.find({ $or: [{ name: { $regex: reg } }, { name_en: { $regex: reg } }] })
+        .lean()
+        .then((results) => {
+            if (results.length === 0) {
+                Model.find({ category: { $regex: reg } })
+                    .lean()
+                    .then((results) => res.render(pageName, { keyword, results }))
+                    .catch((error) => console.log(error))
+            } else {
+                res.render(pageName, { keyword, results })
+            }
+        })
+        .catch((error) => console.log(error))
+}
+
+const findById = (Model, pageName, req, res) => {
+    const id = req.params.id
+    return Model.findById(id)
+        .lean()
+        .then((result) => res.render(pageName, {result}))
+        .catch((error) => console.error(error))
+}
+
+module.exports = { findAll, 
+  findRestaurantByFilter,
+  findById }
--- routes/modules/backstage.js
+++ routes/modules/backstage.js
@@ -3,14 +3,16 @@ const express = require("express")
 const router = express.Router()
 // *引用  model
 const Restaurants = require("../../models/restaurant")
+const { findAll, findRestaurantByFilter } = require("../../controllers/CRUD")
 
 //*顯示後臺管理頁面
 router.get("/", (req, res) => {
-    Restaurants.find()
-        .lean()
-        .sort({ _id: "asc" })
-        .then((restaurants) => res.render("backstage", { restaurants }))
-        .catch((error) => console.error(error))
+    findAll(Restaurants, "backstage", req, res) 
+})
+
+//* Search 資料取得與渲染
+router.get("/search", (req, res) => {
+    findRestaurantByFilter(Restaurants, "backstage", req, res)
 })
 
 // *匯出路由模組
