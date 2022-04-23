7c21797c6e5ceff48c0dbf1c382c55000ef7c699
HungXingYu <clarehung0406@gmail.com>
2022-04-23 11:30:18 +0800

    feat : modify no data result's modal 修改查無資料時顯示的提示訊息，點擊ok時，會返回搜尋主畫面

--- public/javascripts/modalHelper.js
+++ public/javascripts/modalHelper.js
@@ -1,10 +1,26 @@
+//*搜尋
 const searchBar = document.querySelector("#searchBar")
 const restaurantList = document.querySelector("#restaurantList .col")
 
-if(searchBar!==null && restaurantList === null){
-  Swal.fire({
-      icon: "error",
-      title: "查無資料",
-      text: "查無資料，請重新輸入關鍵字在進行搜尋",
-  })
+//*上傳
+const uploadBtn = document.querySelector("#uploadBtn")
+
+
+//#region - 查無資料
+if (searchBar !== null && restaurantList === null) {
+    Swal.fire({
+        title: "查無資料",
+        text: "您輸入的關鍵字查無資料，請重新輸入關鍵字再查詢",
+        icon: "error",
+        confirmButtonColor: "#3085d6",
+        confirmButtonText: "OK"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            const href = location.href.split("?")[0].replace("search", "")
+            window.location.href = href
+        }
+    })
 }
+//#endregion -
+
+
